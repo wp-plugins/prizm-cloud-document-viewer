0a5db52764af299d420ff6cbd6a99e45d94ef458
Accusoft <Accusoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 20:48:07 +0000

    tagging version 1.7
    git-svn-id: https://plugins.svn.wordpress.org/prizm-cloud-document-viewer/tags/1.7@1169659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prizmcloud.php
+++ prizmcloud.php
@@ -6,10 +6,11 @@ Plugin URI: http://prizmviewer.com/
 Description: Prizm Viewer enables you to offer high-speed document viewing without worrying about additional hardware or installing software.  The documents stay on your servers, so you can delete, update, edit and change them anytime. We don't keep copies of your documents, so they are always secure!
 Author: Accusoft <prizmcloud@accusoft.com>
 Author URI: http://www.accusoft.com/
-Version: 1.4
+Version: 1.7
 License: GPL2
 */
-
+define (ACCUSOFT_SERVER, '//api.accusoft.com');
+define (ACCUSOFT_PATH, '/v1/viewer/');
 include_once('prizmcloud-functions.php');
 
 register_activation_hook( __FILE__, 'prizmcloud_plugin_activate' );   
@@ -54,7 +55,9 @@ function prizmcloud_getdocument($atts)
 		'hoverpause' => ''
 	), $atts));
 	$integration = "wordpress";
-	
+
+
+
 	if (strcmp($type,"slideshow") != 0)
 	{
 		$viewerCode = "//connect.ajaxdocumentviewer.com/?key=".$key."&viewertype=".$type."&document=".$document."&viewerheight=".$height."&viewerwidth=".$width."&printButton=".$print."&toolbarColor=".$color."&integration=".$integration;
@@ -82,20 +85,30 @@ add_shortcode('prizmcloud', 'prizmcloud_getdocument');
 // Add Quicktag for Prizm Cloud Admin
 add_action('admin_print_scripts','prizmcloud_admin_print_scripts');
 
-// Add Prizm Cloud Dialog button to Tiny MCEEditor
-add_action('admin_init','prizmcloud_mce_addbuttons');
+// Activate Shortcode to Retrive Document with ACS Viewer
+add_shortcode('acsviewer', 'acsviewer_getdocument');
 
-// Add Prizm Cloud Dialog window to Tiny MCEEditor
-add_action('wp_ajax_prizmcloud_dialog_window', 'prizmcloud_dialog_window');
+// Add ACS Viewer Dialog button to Tiny MCEEditor
+add_action('admin_init','acsviewer_mce_addbuttons');
 
-// Add an Option to Settings Menu for Prizm Cloud
-add_action('admin_menu', 'prizmcloud_settings_page');
+// Add ACS Viewer Dialog window to Tiny MCEEditor
+add_action('wp_ajax_acsviewer_dialog_window', 'acsviewer_dialog_window');
 
-function prizmcloud_settings_page()
+// Add an Option to Settings Menu for ACS Viewer
+add_action('admin_menu', 'acsviewer_settings_page');
+
+add_action('admin_enqueue_scripts', 'enqueue_scripts_styles_admin');
+function enqueue_scripts_styles_admin(){
+    wp_enqueue_media();
+}
+
+
+
+function acsviewer_settings_page()
 {
 	global $prizmcloud_settings_page;
 
-	$prizmcloud_settings_page = add_options_page('Prizm Cloud', 'Prizm Cloud', 'manage_options', basename(__FILE__), 'prizmcloud_settings');
+	$prizmcloud_settings_page = add_options_page('Prizm Viewer', 'Prizm Viewer', 'manage_options', basename(__FILE__), 'prizmcloud_settings');
 
 }
 if (!defined('PRIZMCLOUD_WP_PLUGIN_NAME'))
@@ -106,5 +119,97 @@ function prizmcloud_settings()
 	if ( function_exists('current_user_can') && !current_user_can('manage_options') ) die(t('An error occurred.'));
 	if (! user_can_access_admin_page()) wp_die('You do not have sufficient permissions to access this page');
 
-	require(ABSPATH. 'wp-content/plugins/'. PRIZMCLOUD_WP_PLUGIN_NAME .'/prizmcloud-settings.php');
+	require(ABSPATH. 'wp-content/plugins/'. PRIZMCLOUD_WP_PLUGIN_NAME .'/acsviewer-settings.php');
+}
+
+function acsviewer_getdocument($atts, $content)
+{
+    $licenseKey = get_option('licenseKey');
+    parse_str(html_entity_decode($content), $params);
+
+    if($params['server']) {
+        $server = $params['server'];
+        unset($params['server']);
+    } else {
+        $server = ACCUSOFT_SERVER.ACCUSOFT_PATH;
+    }
+
+    if ($atts) {
+        foreach ($atts as $key => $value) {
+            if (!$params[$key]) {
+                $params[$key] = $value;
+            }
+        }
+    }
+    $params = supportLegacy($params);
+
+    if(!$params['key']) {
+        $params['key'] = $licenseKey;
+    }
+    $viewerCode = $server."?";
+    if($params) {
+        foreach ($params as $key => $value) {
+            $viewerCode .= $key . "=" . $value . "&";
+        }
+    }
+    $viewerCode = rtrim($viewerCode, "&");
+
+    if (strcmp($params['viewertype'],'slideshow') != 0) {
+        if (preg_match('/.+%$/', $params['viewerheight'])) {
+            $iframeHeight = intval($params['viewerheight'])/100 * 800;
+        } else {
+            $iframeHeight = $params['viewerheight'] + 40;
+        }
+        if (preg_match('/.+%$/', $params['viewerwidth'])) {
+            $iframeWidth = $params['viewerwidth'];
+        } else {
+            $iframeWidth = $params['viewerwidth'] + 20;
+        }
+    } else {
+        if (preg_match('/.+%$/', $params['viewerheight'])) {
+            $iframeHeight = intval($params['viewerheight'])/100 * 600;
+        } else {
+            $iframeHeight = $params['viewerheight'] + 20;
+        }
+        if (preg_match('/.+%$/', $params['viewerwidth'])) {
+            $iframeWidth = $params['viewerwidth'];
+        } else {
+            $iframeWidth = $params['viewerwidth'] + 20;
+        }
+    }
+
+    $code = "<iframe src=\"".$viewerCode."\" width=\"".$iframeWidth."\" height=\"".$iframeHeight."\" frameborder =0 seemless></iframe>";
+//    $code = "<iframe src=\"".$viewerCode."\" width=\"100%\" height=\"100%\" frameborder =0 seemless></iframe>";
+
+    return $code;
+}
+
+function supportLegacy($atts)
+{
+    if($atts['type']) {
+        $atts['viewertype'] = $atts['type'];
+        unset($atts['type']);
+    }
+    if ($atts['width']) {
+        $atts['viewerwidth'] = $atts['width'];
+        unset($atts['width']);
+    }
+    if($atts['height']) {
+        $atts['viewerheight'] = $atts['height'];
+        unset($atts['height']);
+    }
+    if($atts['color']) {
+        $atts['lowerToolbarColor'] = $atts['color'];
+        unset($atts['color']);
+    }
+    if ($atts['print'] == "No") {
+        if (strlen($atts['hidden']) > 0) {
+            $atts['hidden'] .= ',print';
+        } else {
+            $atts['hidden'] = 'print';
+        }
+        unset($atts['print']);
+    }
+    return $atts;
+
 }
